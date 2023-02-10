540f5a097bf74df5c0f3489e560b715cd67cd0a1
weekendend <60031519+force278@users.noreply.github.com>
2023-02-10 11:39:34 +0300

    Add files via upload

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+
+function main(isLoggedIn) {
+  if (isLoggedIn){
+    return <Home/>
+  } else {
+    return <Login/>
+  }
+}
 
 function App() {
+  const isLoggedIn = true;
   return (
     <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={main(isLoggedIn)} />
+            <Route path="/login" element={<h1>Вход</h1>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ web/src/screens/Login.js
@@ -0,0 +1,5 @@
+function Login() {
+    return <h1>Войти в аккаунт</h1>
+}
+
+export default Login;
