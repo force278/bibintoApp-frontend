2ded8b755cd64f31a713e593300d7eb4fe291420
weekendend <60031519+force278@users.noreply.github.com>
2023-08-21 13:29:59 +0300

    Add files via upload

--- web/src/components/ProfileEdit/ChangeEditProfile.js
+++ web/src/components/ProfileEdit/ChangeEditProfile.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import { CropperModal } from "./CropperModal";
 import defaultAvatar from "../../assets/img/editProfile/defaultAvatar.png";
 import "../../sass/common.scss"
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 
 const URL_UPLOAD_QUERY = gql`
     query {
@@ -18,9 +18,9 @@ const UPLOAD_AVATAR = gql`
     }
 `;
 
-const SEE_PROFILE_QUERY = gql`
-    query seeProfile($username: String!) {
-        seeProfile(username: $username) {
+const ME_QUERY = gql`
+    query Me {
+        me {
             firstName
             lastName
             username
@@ -34,72 +34,61 @@ const SEE_PROFILE_QUERY = gql`
     }
 `;
 
-const ME_QUERY = gql`
-    query Me {
-        me {
-            username
-        }
-    }
-`;
+
+
 
 export default function ChangeEditProfile() {
+    const loadAvatar = async () => {
+        const imageUrl = uploadData.getUrlUploadPhoto
+
+        const file = inputRef.current.files[0];
+        if (!file) {
+            console.log("Файл не выбран. Загрузка не выполнена.");
+            return;
+        }
+
+        try {
+            const response = await fetch(imageUrl, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                body: file,
+            });
+
+            if (response.ok) {
+                console.log("фото успешно загружена")
+                const img = uploadData.getUrlUploadPhoto.split("?")[0]
+                try {
+                    const uploadResponse = await uploadAvatar({ variables: { file: img } });
+                    if (uploadResponse.data.uploadAvatar) {
+                        console.log("Фото успешно отправлено на сервер.");
+                    } else {
+                        console.error('Ошибка при отправке фотографии на сервер.');
+                    }
+                }catch (error){
+                    console.log('Произошла ошибка', error)
+                }
+
+
+            } else {
+                console.error('Ошибка при загрузке фотографии');
+            }
+        } catch (error) {
+            console.error('Произошла ошибка', error);
+        }
+    }
+
     const [src, setSrc] = useState(null);
     const [modalOpen, setModalOpen] = useState(false);
     const [preview, setPreview] = useState(null);
     const inputRef = useRef(null);
-    const { data: meData } = useQuery(ME_QUERY);
-
-    const { data: profileData, loading } = useQuery(SEE_PROFILE_QUERY, {
-        variables: {username: meData?.me?.username },
-    });
+    const { data: meData , loading} = useQuery(ME_QUERY);
 
 
-    const {data: uploadData} = useQuery(URL_UPLOAD_QUERY);
+    const [getUrl, {data: uploadData}] = useLazyQuery(URL_UPLOAD_QUERY, {onCompleted: loadAvatar});
+
     const [uploadAvatar] = useMutation(UPLOAD_AVATAR);
-
-     const loadAvatar = async () => {
-         if (uploadData && uploadData.getUrlUploadPhoto) {
-             const imageUrl = uploadData.getUrlUploadPhoto
-
-
-             const file = inputRef.current.files[0];
-             if (!file) {
-                 console.log("Файл не выбран. Загрузка не выполнена.");
-                 return;
-             }
-
-             try {
-                 const response = await fetch(imageUrl, {
-                     method: 'PUT',
-                     headers: {
-                         "Content-Type": "multipart/form-data",
-                     },
-                     body: file,
-                 });
-
-                 if (response.ok) {
-                     console.log("фото успешно загружена")
-                     const img = uploadData.getUrlUploadPhoto.split("?")[0]
-                     try {
-                         const uploadResponse = await uploadAvatar({ variables: { file: img } });
-                         if (uploadResponse.data.uploadAvatar) {
-                             console.log("Фото успешно отправлено на сервер.");
-                         } else {
-                             console.error('Ошибка при отправке фотографии на сервер.');
-                         }
-                     }catch (error){
-                         console.log('Произошла ошибка', error)
-                     }
-
-
-                 } else {
-                     console.error('Ошибка при загрузке фотографии');
-                 }
-             } catch (error) {
-                 console.error('Произошла ошибка', error);
-             }
-         }
-    }
 
     const handleInputClick = (e) => {
         e.preventDefault();
@@ -112,7 +101,6 @@ export default function ChangeEditProfile() {
     };
     return (
         <>
-
             <div className="col-8">
                 <div className="row">
                     <div className="d-flex mb-3" style={{marginTop: "33px"}}>
@@ -121,7 +109,7 @@ export default function ChangeEditProfile() {
                                 <img
                                     style={{cursor: "pointer", borderRadius: '50%' }}
                                     onClick={handleInputClick}
-                                    src={preview || profileData?.seeProfile?.avatar || defaultAvatar}
+                                    src={preview || meData?.me?.avatar || defaultAvatar}
                                     alt=""
                                     width="38px"
                                     height="38px"
@@ -143,7 +131,7 @@ export default function ChangeEditProfile() {
                             </div>
                         </div>
                         <div className="d-flex flex-column col-10 ps-4">
-                            <span className="fs-5">{loading ? "Загрузка..." : `${profileData?.seeProfile?.username}`}</span>
+                            <span className="fs-5">{loading ? "Загрузка..." : `${meData?.me?.username}`}</span>
                             <button onClick={handleInputClick} className="text-primary bg-transparent border-0 d-flex justify-content-start ps-0">Загрузите новое фото</button>
                         </div>
                     </div>
@@ -152,7 +140,7 @@ export default function ChangeEditProfile() {
                             <span className="fs-6 pt-2" style={{fontFamily: "Roboto, sans-serif"}}>Имя</span>
                         </div>
                         <div className="col-10 ps-4">
-                            <input type="text" value={profileData?.seeProfile?.firstName} onChange={(e) => e.target.value } className="border border-1 pt-1 pb-1 ps-2 w-50" />
+                            <input type="text" value={meData?.me?.firstName} onChange={(e) => e.target.value } className="border border-1 pt-1 pb-1 ps-2 w-50" />
                             <div className="p-0 m-0 text-secondary">
                                 <div className="mt-2">Чтобы помочь людям найти вашу учетную запись, используйте имя, под которым вас знают.<br/></div>
                                 <div className="mt-3">Вы можете изменить свое имя только два раза в течение 7 дней.</div>
@@ -164,7 +152,7 @@ export default function ChangeEditProfile() {
                             <span className="fs-6 pt-2" style={{fontFamily: "Roboto, sans-serif"}}>Никнейм</span>
                         </div>
                         <div className="col-10 ps-4">
-                            <input type="text" value={profileData?.seeProfile?.username} onChange={(e) => e.target.value }  className="border border-1 pt-1 pb-1 ps-2 w-50" />
+                            <input type="text" value={meData?.me?.username} onChange={(e) => e.target.value }  className="border border-1 pt-1 pb-1 ps-2 w-50" />
                             <div className="mt-2">
                                 <p className="text-secondary">Вы можете снова вернуть свой никнейм в течение <br/> 7 дней.</p>
                             </div>
@@ -188,7 +176,7 @@ export default function ChangeEditProfile() {
                         </div>
                         <div className="col-10 ps-4">
                             <input type="text"
-                                   value={profileData?.seeProfile?.email || 'Данные отсутствуют'}
+                                   value={meData?.me?.email || 'Данные отсутствуют'}
                                    onChange={(e) => e.target.value }
                                    className="border border-1 pt-1 pb-1 ps-2 w-50" />
                         </div>
@@ -198,10 +186,10 @@ export default function ChangeEditProfile() {
                             <span className="fs-6 pt-2" style={{fontFamily: "Roboto, sans-serif"}}>Пол</span>
                         </div>
                         <div className="col-10 ps-4">
-                            <input type="text" value={profileData?.seeProfile?.bio || 'Данные отсутствуют'} onChange={(e) => e.target.value }  className="border border-1 pt-1 pb-1 ps-2 w-50" />
+                            <input type="text" value={meData?.me?.bio || 'Данные отсутствуют'} onChange={(e) => e.target.value }  className="border border-1 pt-1 pb-1 ps-2 w-50" />
                             <div className="mt-5">
                                 <button className="text-white border-0 change_btn"
-                                        onClick={loadAvatar}
+                                        onClick={getUrl}
                                         style={{borderRadius: "4px", background: "#2283F5", padding: "8px 17px", filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",}}>Изменить</button>
                             </div>
                         </div>
@@ -209,6 +197,5 @@ export default function ChangeEditProfile() {
                 </div>
             </div>
         </>
-
     )
 }
