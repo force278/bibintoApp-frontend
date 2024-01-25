5ce54a57ca27e3bce24ca5a08ba1bef8945eb51e
force278 <60031519+force278@users.noreply.github.com>
2024-01-25 18:25:31 +0300

    fix

--- web/src/screens/ViewReport.js
+++ web/src/screens/ViewReport.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react"
-import { Link, Switch, Route, useLocation } from "react-router-dom"
+import { Route } from "react-router-dom"
 import { gql, useQuery } from "@apollo/client"
 import styled from "styled-components"
 import ReportPost from "../components/feed/ReportPost"
 import PageTitle from "../components/PageTitle"
+import CircularProgress from "@mui/material/CircularProgress"
 
 const SEE_REPORT_QUERY = gql`
   query getReport {
@@ -23,27 +23,8 @@ const SEE_REPORT_QUERY = gql`
   }
 `
 
-const StyledSubHeader = styled.div`
-  font-weight: 700;
-  font-size: 20px;
-  font-family: Nexa, sans-serif;
-  line-height: 27px;
-  text-align: center;
-  letter-spacing: 0.1px;
-  padding: 19px 0;
-  color: rgba(0, 0, 0, 0.4);
-  &:hover {
-    cursor: pointer;
-  }
-
-  .active {
-    color: #000;
-  }
-`
-
 function ViewReport() {
   const data = useQuery(SEE_REPORT_QUERY)
-  console.log(data)
 
   return (
     <>
@@ -52,14 +33,31 @@ function ViewReport() {
         <div>
           <Route exact path="/report">
             <div className="mobilePostContainer" style={{ width: "585px" }}>
-              {data?.data?.getReport?.map((report) => (
-                <ReportPost
-                  key={report.id}
-                  id={report.id}
-                  user={report.photo.user}
-                  file={report.photo.file}
-                />
-              ))}
+              {data.loading && data.called ? (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <CircularProgress />
+                </div>
+              ) : (
+                <>
+                  {data.data.getReport == [] ? (
+                    data.data.getReport.map((report) => (
+                      <ReportPost
+                        key={report.id}
+                        id={report.id}
+                        user={report.photo.user}
+                        file={report.photo.file}
+                      />
+                    ))
+                  ) : (
+                    <div>Пока жалоб нет</div>
+                  )}
+                </>
+              )}
             </div>
           </Route>
         </div>
